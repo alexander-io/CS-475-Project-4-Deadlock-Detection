33fa081e681e69149b4027baa0f8c63f9a004010
Xinu Developer Machine <abharris@pugetsound.edu>
2017-05-01 16:30:47 -0700

    430pm monday

--- system/main.c
+++ system/main.c
@@ -103,19 +103,19 @@ void	philosopher(uint32 phil_id)
 
 int	main(uint32 argc, uint32 *argv)
 {
+	kprintf("hello world\n");
+	// int i;
+	// for(i=0;i<N;i++){
+	// 	locks[i] = lock_create();
 
-	int i;
-	for(i=0;i<N;i++){
-		locks[i] = lock_create();
-
-	}
-	print_lock = lock_create();
+	// }
+	// print_lock = lock_create();
 
 	//do not change
-	ready(create((void*) philosopher, INITSTK, 15, "Ph1", 1, 0), FALSE);
-	ready(create((void*) philosopher, INITSTK, 15, "Ph2", 1, 1), FALSE);
-	ready(create((void*) philosopher, INITSTK, 15, "Ph3", 1, 2), FALSE);
-	ready(create((void*) philosopher, INITSTK, 15, "Ph4", 1, 3), FALSE);
-	ready(create((void*) philosopher, INITSTK, 15, "Ph5", 1, 4), FALSE);
+	// ready(create((void*) philosopher, INITSTK, 15, "Ph1", 1, 0), FALSE);
+	// ready(create((void*) philosopher, INITSTK, 15, "Ph2", 1, 1), FALSE);
+	// ready(create((void*) philosopher, INITSTK, 15, "Ph3", 1, 2), FALSE);
+	// ready(create((void*) philosopher, INITSTK, 15, "Ph4", 1, 3), FALSE);
+	// ready(create((void*) philosopher, INITSTK, 15, "Ph5", 1, 4), FALSE);
 	return 0;
 }
